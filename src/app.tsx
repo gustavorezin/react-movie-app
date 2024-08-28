7d2d93e3009a6032f873d0e884a3b6cb4e844a35
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-08-28 17:34:05 -0300

    refactor: chamando page `PopularMovies` no app

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+import { PopularMovies } from "./pages/popular-movies";
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <PopularMovies />;
 }
